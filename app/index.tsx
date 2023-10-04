6ac7d7b8b663e97bdc704e5d40081fdec247ac6e
SergiyPysanenko <SergiyPysanenko@gmail.com>
2023-10-04 19:06:40 +0300

    add css and vars

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+export default function Home(): JSX.Element {
+  return (
+    <div className="div">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, rem!
+      Sequi ullam, inventore enim, dolorem perferendis minima velit sapiente
+      nulla nesciunt rerum sint ea cupiditate accusantium quas asperiores
+      assumenda distinctio!
+    </div>
+  );
+}
