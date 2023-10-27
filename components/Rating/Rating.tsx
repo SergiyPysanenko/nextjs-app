9ea2efeeaa3c0859b810e3be6b570079e7acbcca
SergiyPysanenko <SergiyPysanenko@gmail.com>
2023-10-27 19:55:06 +0300

    Add prettierrc

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -1,26 +1,24 @@
-import { RatingProps } from "./Rating.props";
-import s from "./Rating.module.css";
-import cn from "classnames";
-import { JSXElementConstructor, useState } from "react";
-import {AiOutlineStar} from 'react-icons/ai'
+import { RatingProps } from './Rating.props';
+import s from './Rating.module.css';
+import cn from 'classnames';
+import { useState } from 'react';
+import { AiOutlineStar } from 'react-icons/ai';
 
 export const Rating = ({
-  isEditable = "false",
+  isEditable = false,
   rating,
   setRaiting,
-  className,
   ...props
 }: RatingProps): JSX.Element => {
-    const [ratingArray, setRatingArray] = useState<JSX.Element[]>(
-    newArray(5).fill(<></>);
-    
-    const constructArray = (currentRating: number) => {
-      const updateArray = ratingArray.map((r: JSX.Element, i: number) =>)
-      return (
-        < AiOutlineStar />
-      )
-       
+  const [ratingArray, setRatingArray] = useState<JSX.Element[]>(
+    new Array(5).fill(<></>),
   );
+
+  const constructArray = (currentRating: number) => {
+    const updateArray = ratingArray.map((r: JSX.Element, i: number) => {
+      return <AiOutlineStar className={cn(s.star)} />;
+    });
+  };
 
   return <div {...props}></div>;
 };
